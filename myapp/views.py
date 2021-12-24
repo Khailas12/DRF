14231738ee02192953977e1361a909dd9a47eef9
Khailas R <khailas303@gmail.com>
2021-12-24 21:04:05 +0530

    CRUD

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -6,18 +7,29 @@ from .serializers import SnippetSerializer
 
 
 @csrf_exempt
-def snippet_list(request):
+def snippet_list(request, pk):  # CRUD
     
-    if request.method == 'Post':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
     
-    elif request.method == 'GET':
+    if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
