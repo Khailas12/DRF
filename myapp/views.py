a7a1b13a77c53e436d1d173b021fc1935978db4a
Khailas R <khailas303@gmail.com>
2021-12-24 21:31:51 +0530

    simpified

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework.renderers import JSONRenderer
 
 
 class JSONResponse(HttpResponse):
