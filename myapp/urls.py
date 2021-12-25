e679dd2b9cccfd116cd6faf9074b809d65170526
Khailas R <khailas303@gmail.com>
2021-12-25 14:04:38 +0530

    updated
    request error fixed. working..

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url('', views.SnippetList.as_view()),
-    url('<int:pk>/', views.snippt_detail),
+    url('<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)       # http://127.0.0.1:8000/snippets/.json
--- myapp/views.py
+++ myapp/views.py
@@ -1,19 +1,8 @@
-from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from rest_framework import status
 from rest_framework.views import APIView
 from .models import Snippet
 from .serializers import SnippetSerializer
-
-
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
 
 """
@@ -23,7 +12,6 @@ format=None -> api will be able to handle URLs of any format like json, eg: http
 request.POST  # Only handles form data.  Only works for 'POST' method.
 request.data  # Handles arbitrary data.  Works for 'POST', 'PUT' and 'PATCH' methods.
 """
-
 
 class SnippetList(APIView): # class based
     
@@ -43,10 +31,9 @@ class SnippetList(APIView): # class based
     
 class SnippetDetail(APIView):   # RUD
     
-    
     def get_object(self, pk):
         try:
-            return Snippet.objects.all()    
+            return Snippet.objects.get(pk=pk)  
         except Snippet.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
@@ -63,12 +50,34 @@ class SnippetDetail(APIView):   # RUD
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk, format=None):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+""" Function based
+
+@api_view(['POST', 'GET'])
+def snippet_list(request, format=None):  # create
+    
+    if request.method == 'POST':
+        serializer = SnippetSerializer(data=request.DATA)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+"""     
 
 """
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -95,23 +104,3 @@ def snippt_detail(request, pk, format=None):  # CRUD
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 """
-    
-    
-""" Function based
-@api_view(['POST', 'GET'])
-def snippet_list(request, format=None):  # create
-    
-    if request.method == 'POST':
-        serializer = SnippetSerializer(data=request.DATA)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-"""     
